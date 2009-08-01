183b5b4998ae6787f1f2ef94666f9ab3b118828a
David O'Toole <dto@gnu.org>
2009-08-01 07:21:53 -0400

    works

--- ldoc.el
+++ ldoc.el
@@ -1,5 +1,6 @@
 ;;; ldoc.el --- extract and format documentation from lisp files
 
+
 ;; Copyright (C) 2009  David O'Toole
 
 ;; Author: David O'Toole <dto@gnu.org>
@@ -26,142 +27,33 @@
 
 ;;; Code:
 
-(require 'cl)
+;;; Prerequisites
 
-;;; Defining groups of files
+;; We require radio-mode (another of my projects) for defining groups
+;; of files to process and index.
 
-;; A group is a set of files that are handled together.
+;; http://github.com/dto/radio
 
-(defstruct ldoc-group 
-  ;; Required slots.
-  name ;; String name of group. 
-  base-directory ;; Directory to scan for files.
-  format ;; A keyword like :lisp, :text, :org
-  include ;; Regexp or list of files to include
-  ;; Optional slots
-  description ;; Optional string description.
-  exclude  ;; Regexp or list of files to exclude
-  etags-file  ;; etags filename (relative to base-directory).
-              ;; The default is TAGS.
-  etags-arguments ;; List of argument strings for etags.
-  ;; Other slots
-  selected-p ;; When non-nil, multi-group operations apply to this group. 
-  files ;; Cached list of files in this group; updated on each file scan.
-        ;; See also `ldoc-scan-group-files'.
-  )
+(require 'radio) 
 
-(defun ldoc-match-regexp-or-list (filename regexp-or-list)
-  "Return non-nil if FILENAME is either a member of the list (or
-matches the regexp) REGEXP-OR-LIST."
-  (if (null regexp-or-list)
-      nil
-      (etypecase regexp-or-list
-	(string (string-match regexp-or-list filename))
-	(list (member filename regexp-or-list)))))
+;; We also need Org for output formatting.
 
-(defun ldoc-filter-files (files regexp-or-list)
-  "Return FILES with any matching files removed.  If the second
-argument is a regular expression, remove files matching the
-regexp. If it's a list, remove files matching any filename in the
-list. See also `ldoc-match-regexp-or-list'."
-  (labels ((match (filename)
-	     (ldoc-match-regexp-or-list filename regexp-or-list)))
-    (remove-if #'match files)))
+(require 'org)
 
-(defun ldoc-get-group-files (group)
-  "Obtain a list of all the available files in the group GROUP."
-  (let ((dir (ldoc-group-base-directory group)))
-    (labels ((expand (filename)
-	       (expand-file-name filename dir)))
-      (let* ((include (ldoc-group-include group))
-	     (files (etypecase include
-		      (string (directory-files dir nil include))
-		      (list (mapcar #'expand include)))))
-	(mapcar #'expand 
-		(remove-duplicates 
-		 (ldoc-filter-files files (ldoc-group-exclude group))
-		 :test 'equal))))))
+;;; Test forms
 
-(defun ldoc-load-group-files (group)
-  (setf (ldoc-group-files group)
-	(ldoc-get-group-files group)))
+(defvar *ldoc-test* 
+  (make-radio-group :name "rlx"
+		   :base-directory "~/rlx/"
+		   :include "*\\.lisp$"))
 
-(defun ldoc-create-group (&rest forms)
-  (let ((group (apply #'make-ldoc-group forms)))
-    (prog1 group 
-      (ldoc-load-group-files group))))
+;;; Definitions
 
-;;; Groups table
+(defstruct ldoc-definition name type nextform docstring source-code file)
 
-(defvar *ldoc-groups* nil)
-
-(defun ldoc-init-groups ()
-  (interactive)
-  (setf *ldoc-groups* (make-hash-table :test 'equal)))
-
-(when (null *ldoc-groups*)
-  (ldoc-init-groups))
-
-(defun ldoc-add-group (&rest args)
-  (let ((group (apply #'ldoc-create-group args)))
-    (setf (gethash (ldoc-group-name group)
-		   *ldoc-groups*)
-	  group)))
-
-(defun ldoc-delete-group (name)
-  (remhash name *ldoc-groups*))
-
-(defun* ldoc-scan-group-files (&optional (group-name (ldoc-choose-group)))
-  (ldoc-load-group-files (gethash group-name *ldoc-groups*))
-  (message "Scanned group %s for files." group-name))
-
-;;; Selecting and deselecting groups
-
-(defun* ldoc-choose-group () 
-  (completing-read "Choose a group: " *ldoc-groups* nil :require-match))
-
-(defun* ldoc-select-group (&optional (group-name (ldoc-choose-group)))
-  (interactive)
-  (let ((group (gethash group-name *ldoc-groups*)))
-    (prog1 group 
-      (setf (ldoc-group-selected-p group) t))))
-
-(defun* ldoc-deselect-group (&optional (group-name (ldoc-choose-group)))
-  (interactive)
-  (let ((group (gethash group-name *ldoc-groups*)))
-    (prog1 group 
-      (setf (ldoc-group-selected-p group) nil))))
-
-(defun ldoc-get-selected-groups ()
-  (let (groups)
-    (maphash #'(lambda (name group)
-		 (declare (ignore name))
-		 (when (ldoc-group-selected-p group)
-		   (push group groups)))
-	     *ldoc-groups*)
-    groups))
-
-;;; Printing information about groups
-
-(defun* ldoc-describe-group (&optional (group-name (ldoc-choose-group)))
-  (interactive)
-  (let ((group (gethash group-name *ldoc-groups*)))
-    (message (ldoc-group-description group))))
-
-(defun ldoc-show-selected-groups ()
-  (interactive)
-  (maphash #'(lambda (name group)
-	       (ldoc-describe-group name))
-	   *ldoc-groups*))
-
-;;; Scanning definitions and docstrings
+;;; Scanning definitions 
 
 ;; A definition line is defined by the regexp `ldoc-def-regexp' below. 
-;; A definition's docstring (if any) is found like this: 
-;; 
-;;   - Scan lines for any definition line
-;;   - Find and parse definition
-;;   - Scan following lines
 
 (defvar ldoc-def-types '("defadvice" "defalias" "defgeneric"
 			   "defmacro" "defmethod" "defun"
@@ -194,25 +86,68 @@ list. See also `ldoc-match-regexp-or-list'."
 			 "prototype" "method" "cell"))
 
 (defvar ldoc-def-regexp (concat "(" 
+				;; definition type 
 				"\\(" 
 				(regexp-opt ldoc-def-types)
 				"\\)"
-				" \\([^\n ]*\\)"))
+				;; name being defined
+				" \\([^\n ]*\\)"
+				;; scan to beginning of next form
+				"\\([[:space:]]+\\)"))
+
+(defun ldoc-format-type (type)
+  (let ((pos (position-if (lambda (x)
+			    (string= type x))
+			  ldoc-def-types)))
+    (when (numberp pos)
+	  (nth pos ldoc-def-names))))
+
+(defvar ldoc-blank-line-regexp "^[[:space:]]*$")
+
+;; (ldoc-next-definition)
+
+(defvar ldoc-doc-begin-regexp "^\\([[:space:]]*\\)\\(\"\\)")
+
+(defun ldoc-next-blank-line-position ()
+  (save-excursion
+    (when (re-search-forward ldoc-blank-line-regexp nil t)
+      (point-at-bol))))
+
+(defun ldoc-next-docstring ()
+  "Find the next docstring (if any) with the following procedure:
+
+  - Scan lines for any definition line
+  - Find and parse definition
+  - Scan following lines until blank line 
+    (i.e. empty or consisting solely of whitespace)
+    looking for docstring
+  - If found, use elisp #'read to snarf string
+
+This means functions should be separated by at least a blank
+line, and docstrings should always start on a fresh line."
+  (when (re-search-forward ldoc-doc-begin-regexp
+			   (or (ldoc-next-blank-line-position)
+			       (point-max))
+			   t)
+    ;; jump to opening double-quote of docstring
+    (goto-char (match-beginning 2))
+    (read (current-buffer))))
+
+(defun ldoc-next-definition ()
+  (when (re-search-forward ldoc-def-regexp nil t)
+    (make-ldoc-definition :type (ldoc-format-type (match-string-no-properties 1))
+			  :name (match-string-no-properties 2)
+			  :nextform (save-excursion 
+				      (goto-char (match-beginning 3))
+				      (read (current-buffer)))
+			  :docstring (ldoc-next-docstring)
+			  :file (buffer-file-name (current-buffer)))))
 
 
-;; (defun golisp-write-def (target line anchor-p)	
-;;   (string-match golisp-def-regexp line)
-;;   (let ((type (match-string 1 line)))
-;;     (when type
-;;       (let ((pos (position-if (lambda (x)
-;; 				(string= type x))
-;; 			      golisp-def-types)))
-;; 	(when pos
-;; 	  (if anchor-p
-;; 	      (concat "(" type " " target " ")
-;; 	    (list 
-;; 	     (concat "<" "(" (nth pos golisp-def-names) " " target ")" ">")
-;; 	     (concat "(" type " " target " "))))))))
+    
+;;; Producing one big indexed HTML doc file for a group.
+
+
 
 
 
